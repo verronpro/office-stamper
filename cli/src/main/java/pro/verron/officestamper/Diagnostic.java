765ce1aff8e7cb2b77ebb3602beba5bf767ac500
Joseph Verron <joseph@verron.pro>
2025-03-05 23:17:35 +0800

    refactor(cli): migrate and modularize CLI functionality into independent module
    Streamline project structure by separating CLI logic from examples module, enhancing maintainability and clarity. Replace "examples" with "cli" module, encapsulating diagnostic context and resource handling.

---
+++ cli/src/main/java/pro/verron/officestamper/Diagnostic.java
@@ -0,0 +1,101 @@
+package pro.verron.officestamper;
+
+import java.io.InputStream;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+import static java.util.stream.Collectors.toMap;
+
+public final class Diagnostic {
+
+    private static final Logger logger = Utils.getLogger();
+    private final LocalDate date;
+    private final String user;
+    private final Map<String, String> userPreferences;
+    private final Map<String, String> jvmProperties;
+    private final Map<String, String> environmentVariables;
+
+    private Diagnostic() {
+        this.date = LocalDate.now();
+        this.user = System.getenv("USERNAME");
+        this.userPreferences = extractUserPreferences();
+        this.jvmProperties = extractJvmProperties();
+        this.environmentVariables = extractEnvironmentVariables();
+    }
+
+    private static Map<String, String> extractUserPreferences() {
+        var preferenceRoot = Preferences.userRoot();
+        var preferenceKeys = extractPreferenceKeys(preferenceRoot);
+        var entries = preferenceKeys.stream()
+                                    .collect(toMap(key -> key, k -> preferenceRoot.get(k, "<null>")));
+        return Map.copyOf(entries);
+    }
+
+    private static Map<String, String> extractJvmProperties() {
+        var properties = System.getProperties();
+        var propertyNames = properties.stringPropertyNames();
+        var entries = propertyNames.stream()
+                                   .collect(toMap(k -> k, properties::getProperty));
+        return Map.copyOf(entries);
+    }
+
+    private static Map<String, String> extractEnvironmentVariables() {
+        var env = System.getenv();
+        return Map.copyOf(env);
+    }
+
+    private static List<String> extractPreferenceKeys(Preferences preferenceRoot) {
+        try {
+            return Arrays.asList(preferenceRoot.keys());
+        } catch (BackingStoreException e) {
+            logger.log(Level.WARNING, "Failed to list the preference keys", e);
+            return List.of("failed-to-list-preference-keys");
+        }
+    }
+
+    static Object context() {
+        logger.info("Create a context with system environment variables, jvm properties, and user preferences");
+        var diagnosticMaker = new Diagnostic();
+        var map = new TreeMap<String, Object>();
+        map.put("reportDate", diagnosticMaker.date());
+        map.put("reportUser", diagnosticMaker.user());
+        map.put("environment",
+                diagnosticMaker.environmentVariables()
+                               .entrySet());
+        map.put("properties",
+                diagnosticMaker.jvmProperties()
+                               .entrySet());
+        map.put("preferences",
+                diagnosticMaker.userPreferences()
+                               .entrySet());
+        return map;
+    }
+
+    public LocalDate date() {return date;}
+
+    public String user() {return user;}
+
+    private Map<String, String> environmentVariables() {
+        return environmentVariables;
+    }
+
+    private Map<String, String> jvmProperties() {
+        return jvmProperties;
+    }
+
+    private Map<String, String> userPreferences() {
+        return userPreferences;
+    }
+
+    static InputStream template() {
+        logger.info("Load the internally packaged 'Diagnostic.docx' template resource");
+        return Utils.streamResource("Diagnostic.docx");
+    }
+}

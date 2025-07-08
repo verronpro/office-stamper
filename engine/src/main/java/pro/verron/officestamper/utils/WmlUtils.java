dab07ef33e7a5ca803c6b44ee8a22ba267242de3
Joseph Verron <joseph@verron.pro>
2025-07-08 21:53:08 +0800

    docs: add detailed Javadoc comments to WmlUtils utility methods
    Enhance code readability

--- engine/src/main/java/pro/verron/officestamper/utils/WmlUtils.java
+++ engine/src/main/java/pro/verron/officestamper/utils/WmlUtils.java
@@ -19,23 +19,43 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Predicate;
 
+/**
+ * Utility class with methods to help in the interaction with WordprocessingMLPackage documents
+ * and their elements, such as comments, parents, and child elements.
+ */
 public final class WmlUtils {
     private WmlUtils() {
         throw new OfficeStamperException("Utility class shouldn't be instantiated");
     }
 
-    public static <T> Optional<T> getFirstParentWithClass(Child child, Class<T> aClass, int depth) {
+    /**
+     * Attempts to find the first parent of a given child element that is an instance of the specified class within
+     * the defined search depth.
+     *
+     * @param child the child element from which the search for a parent begins.
+     * @param clazz the class type to match for the parent
+     * @param depth the maximum amount levels to traverse up the parent hierarchy
+     * @param <T> the type of the parent class to search for
+     * @return an Optional containing the first parent matching the specified class, or an empty Optional if no match found.
+     */
+    public static <T> Optional<T> getFirstParentWithClass(Child child, Class<T> clazz, int depth) {
         var parent = child.getParent();
         var currentDepth = 0;
         while (currentDepth <= depth) {
             currentDepth++;
             if (parent == null) return Optional.empty();
-            if (aClass.isInstance(parent)) return Optional.of(aClass.cast(parent));
+            if (clazz.isInstance(parent)) return Optional.of(clazz.cast(parent));
             if (parent instanceof Child next) parent = next.getParent();
         }
         return Optional.empty();
     }
 
+    /**
+     * Extracts a list of comment elements from the specified WordprocessingMLPackage document.
+     *
+     * @param document the WordprocessingMLPackage document from which to extract comment elements
+     * @return a list of Child objects representing the extracted comment elements
+     */
     public static List<Child> extractCommentElements(WordprocessingMLPackage document) {
         var commentFinder = new CommentFinder();
         TraversalUtil.visit(document, true, commentFinder);
@@ -82,6 +102,17 @@ public final class WmlUtils {
         };
     }
 
+
+    /**
+     * Removes the specified child element from its parent container.
+     * Depending on the type of the parent element, the removal process
+     * is delegated to the appropriate helper method. If the child is
+     * contained within a table cell and the cell is empty after removal,
+     * an empty paragraph is added to the cell.
+     *
+     * @param child the child element to be removed
+     * @throws OfficeStamperException if the parent of the child element is of an unexpected type
+     */
     public static void remove(Child child) {
         switch (child.getParent()) {
             case ContentAccessor parent -> remove(parent, child);
@@ -95,10 +126,15 @@ public final class WmlUtils {
         }
     }
 
-    private static void remove(SdtRun parent, Child child) {
-        parent.getSdtContent()
-              .getContent()
-              .remove(child);
+    private static void remove(ContentAccessor parent, Child child) {
+        var siblings = parent.getContent();
+        var iterator = siblings.listIterator();
+        while (iterator.hasNext()) {
+            if (equals(iterator.next(), child)) {
+                iterator.remove();
+                break;
+            }
+        }
     }
 
     @SuppressWarnings("SuspiciousMethodCalls")
@@ -113,15 +149,10 @@ public final class WmlUtils {
               .remove(child);
     }
 
-    private static void remove(ContentAccessor parent, Child child) {
-        var siblings = parent.getContent();
-        var iterator = siblings.listIterator();
-        while (iterator.hasNext()) {
-            if (equals(iterator.next(), child)) {
-                iterator.remove();
-                break;
-            }
-        }
+    private static void remove(SdtRun parent, Child child) {
+        parent.getSdtContent()
+              .getContent()
+              .remove(child);
     }
 
     private static boolean equals(Object o1, Object o2) {

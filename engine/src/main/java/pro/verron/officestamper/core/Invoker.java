75c0df42e8c3e496eb92a8a78d7a96b07dfc5319
Joseph Verron <joseph@verron.pro>
2025-07-10 08:23:36 +0800

    docs: enhance Javadoc comments across core classes
    Improve documentation by adding and refining Javadoc comments in key core classes, such as `Invoker`, `CommentProcessorRegistry`, and `PlaceholderReplacer`. Clarify method behaviors, parameters, return types, and internal logic for better usability and maintainability.

--- engine/src/main/java/pro/verron/officestamper/core/Invoker.java
+++ engine/src/main/java/pro/verron/officestamper/core/Invoker.java
@@ -9,20 +9,31 @@ import static java.util.Arrays.asList;
 
 
 /**
- * The Invoker class encapsulates an object and a method to be invoked on that object. It provides a way to
- * execute
- * the specified method with given arguments within a certain evaluation context.
+ * Represents an invoker that encapsulates the name, arguments, and executor of a method
+ * to be invoked.
+ * It acts as a wrapper to facilitate method invocation with specific parameters,
+ * and a dedicated execution strategy.
  */
 public record Invoker(String name, Invokers.Args args, MethodExecutor executor) {
 
     /**
-     * @param obj    the target object on which the method is to be invoked
-     * @param method the method to be invoked on the target object
+     * Constructs an {@code Invoker} instance by extracting the method name, parameter types,
+     * and creating a corresponding {@link ReflectionExecutor} for the provided object and method.
+     *
+     * @param obj    the object on which the method will be invoked.
+     * @param method the method to be invoked, including its name and parameter types.
      */
     public Invoker(Object obj, Method method) {
         this(method.getName(), asList(method.getParameterTypes()), new ReflectionExecutor(obj, method));
     }
 
+    /**
+     * Constructs an {@code Invoker} instance using the provided method name, argument types, and executor.
+     *
+     * @param name     the name of the method to be invoked.
+     * @param args     the list of argument types required for the method invocation.
+     * @param executor the executor responsible for executing the method.
+     */
     public Invoker(String name, List<Class<?>> args, MethodExecutor executor) {
         this(name, new Invokers.Args(args), executor);
     }

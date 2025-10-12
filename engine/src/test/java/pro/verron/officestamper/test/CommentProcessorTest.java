aed8ead09d72dd1ef7659527219a8de23bb2cbb7
Jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2025-10-12 19:54:23 +0200

    add second example

--- engine/src/test/java/pro/verron/officestamper/test/CommentProcessorTest.java
+++ engine/src/test/java/pro/verron/officestamper/test/CommentProcessorTest.java
@@ -25,6 +25,23 @@ public class CommentProcessorTest {
                 """;
 
         assertEquals(expected, actual);
-        // Implement test logic for CommentProcessor here
+    }
+
+    @Test
+    void notWorking2() {
+        var stamperConfiguration = standardWithPreprocessing();
+        var stamper = new TestDocxStamper<>(stamperConfiguration);
+        var templateStream = TestUtils.getResource(Path.of("ReplaceWithCommentNotWorking2.docx"));
+
+        var context = Map.of("name", "Homer Simpson");
+
+
+        var actual = stamper.stampAndLoadAndExtract(templateStream, context);
+
+        var expected = """
+                Homer Simpson
+                """;
+
+        assertEquals(expected, actual);
     }
 }

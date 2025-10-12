685e2641c46de2d44f303b82fce7bd0c2eb9cd2e
Jonathan Sarrazin <jonathan.sarrazin@prodyna.com>
2025-10-12 19:49:50 +0200

    comment replacement not working example

---
+++ engine/src/test/java/pro/verron/officestamper/test/CommentProcessorTest.java
@@ -0,0 +1,30 @@
+package pro.verron.officestamper.test;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static pro.verron.officestamper.preset.OfficeStamperConfigurations.standardWithPreprocessing;
+
+public class CommentProcessorTest {
+    @Test
+    void notWorking1() {
+        var stamperConfiguration = standardWithPreprocessing();
+        var stamper = new TestDocxStamper<>(stamperConfiguration);
+        var templateStream = TestUtils.getResource(Path.of("ReplaceWithCommentNotWorking1.docx"));
+
+        var context = Map.of("name", "Homer Simpson");
+
+
+        var actual = stamper.stampAndLoadAndExtract(templateStream, context);
+
+        var expected = """
+                [Normal] This variable name should be resolved to the value Homer Simpson.<jc=LEFT>
+                """;
+
+        assertEquals(expected, actual);
+        // Implement test logic for CommentProcessor here
+    }
+}

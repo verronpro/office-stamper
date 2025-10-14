16fad28ed2c6c806bcc15e8e5d02ad9231fc3d75
Joseph Verron <joseph@verron.pro>
2025-10-14 19:46:35 +0800

    test: remove unused test case `notWorking2` from CommentProcessorTest
    - Eliminated the redundant test method `notWorking2` to clean up the test class.
    - Streamlined assertions by removing unused expectations and context setup.

--- engine/src/test/java/pro/verron/officestamper/test/CommentProcessorTest.java
+++ engine/src/test/java/pro/verron/officestamper/test/CommentProcessorTest.java
@@ -24,23 +24,6 @@ public class CommentProcessorTest {
                 [Normal] This variable name should be resolved to the value Homer Simpson.<jc=LEFT>
                 """;
 
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void notWorking2() {
-        var stamperConfiguration = standardWithPreprocessing();
-        var stamper = new TestDocxStamper<>(stamperConfiguration);
-        var templateStream = TestUtils.getResource(Path.of("ReplaceWithCommentNotWorking2.docx"));
-
-        var context = Map.of("name", "Homer Simpson");
-
-
-        var actual = stamper.stampAndLoadAndExtract(templateStream, context);
-
-        var expected = """
-                Homer Simpson
-                """;
 
         assertEquals(expected, actual);
     }

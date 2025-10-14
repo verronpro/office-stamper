9b9cdffc1454790d4b90ee7ba6a8d8853fb0d2d4
Joseph Verron <joseph@verron.pro>
2025-10-14 19:53:48 +0800

    refactor: remove logger from ReplaceWithProcessor and adapt logic to leverage StandardParagraph's replace method for improved generality

--- engine/src/main/java/pro/verron/officestamper/preset/processors/replacewith/ReplaceWithProcessor.java
+++ engine/src/main/java/pro/verron/officestamper/preset/processors/replacewith/ReplaceWithProcessor.java
@@ -1,14 +1,13 @@
 package pro.verron.officestamper.preset.processors.replacewith;
 
 import org.docx4j.wml.R;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.lang.Nullable;
 import pro.verron.officestamper.api.AbstractCommentProcessor;
 import pro.verron.officestamper.api.CommentProcessor;
 import pro.verron.officestamper.api.DocxPart;
 import pro.verron.officestamper.api.ParagraphPlaceholderReplacer;
 import pro.verron.officestamper.preset.CommentProcessorFactory;
+import pro.verron.officestamper.utils.WmlFactory;
 
 import java.util.List;
 import java.util.function.Function;
@@ -27,8 +26,6 @@ import static pro.verron.officestamper.utils.WmlFactory.newText;
 public class ReplaceWithProcessor
         extends AbstractCommentProcessor
         implements CommentProcessorFactory.IReplaceWithProcessor {
-
-    private static final Logger log = LoggerFactory.getLogger(ReplaceWithProcessor.class);
 
     private final Function<R, List<Object>> nullSupplier;
 
@@ -69,17 +66,17 @@ public class ReplaceWithProcessor
      */
     @Override public void replaceWordWith(@Nullable String expression) {
         R run = this.getCurrentRun();
-        if (run == null) { // TODO Find a way to never call this method when not on a run comment
-            log.info("Impossible to put expression {} in a null run", expression);
-            return;
+        if (run != null) { //TODO Remove the run-based secundary case that becomes redundant
+            var target = expression != null ? List.of(newText(expression)) : nullSupplier.apply(run);
+            var runContent = run.getContent();
+            runContent.clear();
+            runContent.addAll(target);
         }
-
-        var target = expression != null ?
-                List.of(newText(expression)) :
-                nullSupplier.apply(run);
-
-        var runContent = run.getContent();
-        runContent.clear();
-        runContent.addAll(target);
+        else {
+            var comment = this.getCurrentCommentWrapper();
+            var from = comment.getCommentRangeStart();
+            var to = comment.getCommentRangeEnd();
+            getParagraph().replace(from, to, WmlFactory.newRun(expression));
+        }
     }
 }

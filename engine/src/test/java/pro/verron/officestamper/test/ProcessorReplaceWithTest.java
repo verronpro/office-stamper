1d2562690bdc3dd7adc378ce0bde2d0f35fcd3d3
Joseph Verron <joseph@verron.pro>
2025-10-14 21:01:53 +0800

    test: add DisplayName annotation and streamline ProcessorReplaceWithTest
    - Added `@DisplayName` annotation to enhance test readability and traceability.
    - Refactored imports for better clarity.
    - Simplified expected string assertion and resource retrieval.
    - Renamed and deleted test resources for improved alignment with test cases.

--- engine/src/test/java/pro/verron/officestamper/test/ProcessorReplaceWithTest.java
+++ engine/src/test/java/pro/verron/officestamper/test/ProcessorReplaceWithTest.java
@@ -1,5 +1,6 @@
 package pro.verron.officestamper.test;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.nio.file.Path;
@@ -7,24 +8,17 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static pro.verron.officestamper.preset.OfficeStamperConfigurations.standardWithPreprocessing;
+import static pro.verron.officestamper.test.TestUtils.getResource;
 
 class ProcessorReplaceWithTest {
-    @Test
+    @Test @DisplayName("#585_ReplaceWith")
     void notWorking1() {
         var stamperConfiguration = standardWithPreprocessing();
         var stamper = new TestDocxStamper<>(stamperConfiguration);
-        var templateStream = TestUtils.getResource(Path.of("ReplaceWithCommentNotWorking1.docx"));
-
+        var templateStream = getResource(Path.of("ReplaceWithCommentNotWorking1.docx"));
         var context = Map.of("name", "Homer Simpson");
-
-
         var actual = stamper.stampAndLoadAndExtract(templateStream, context);
-
-        var expected = """
-                [Normal] This variable name should be resolved to the value Homer Simpson.<jc=LEFT>
-                """;
-
-
+        var expected = "[Normal] This variable name should be resolved to the value Homer Simpson.<jc=LEFT>\n";
         assertEquals(expected, actual);
     }
 }

4b43618feae4d7beda9eff236d023b066d11af6e
Joseph Verron <joseph@verron.pro>
2025-11-30 12:43:36 +0800

    refactor(tests): redesign CustomProcessor tests and remove outdated CustomCommentProcessor
    - Removed CustomCommentProcessor and ICustomCommentProcessor as they were no longer relevant
    - Introduced CustomProcessor and ICustomProcessor with updated design and functionality
    - Added new CustomProcessorTests to verify custom processor integration and behavior
    - Modified DefaultTests to exclude obsolete custom processor-related tests

---
+++ engine/src/test/java/pro/verron/officestamper/test/CustomProcessorTests.java
@@ -0,0 +1,78 @@
+package pro.verron.officestamper.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import pro.verron.officestamper.api.CommentProcessor;
+import pro.verron.officestamper.api.PlaceholderReplacer;
+import pro.verron.officestamper.api.ProcessorContext;
+
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.argumentSet;
+import static pro.verron.officestamper.preset.OfficeStamperConfigurations.standard;
+import static pro.verron.officestamper.test.ContextFactory.mapContextFactory;
+import static pro.verron.officestamper.test.ContextFactory.objectContextFactory;
+import static pro.verron.officestamper.test.TestUtils.getResource;
+import static pro.verron.officestamper.utils.WmlFactory.newRun;
+
+/// Test class for verifying the integration and functionality of Custom Comment Processors within the stamping process
+/// of DOCX documents.
+@DisplayName("") class CustomProcessorTests {
+
+    private static Stream<Arguments> factories() {
+        return Stream.of(argumentSet("Object-based", objectContextFactory()),
+                argumentSet("Map-based", mapContextFactory()));
+    }
+
+    @MethodSource("factories")
+    @ParameterizedTest(name = "Should allow to inject custom processors ({argumentSetName})")
+    void should_allow_custom_processors_injection(ContextFactory factory) {
+        var config = standard().addCommentProcessor(ICustomProcessor.class, CustomProcessor::new);
+        var template = getResource(Path.of("CustomCommentProcessorTest.docx"));
+        var expected = """     
+                == Custom Comment Processor Test
+                
+                Visited
+                This paragraph is untouched.
+                Visited
+                """;
+        var stamper = new TestDocxStamper<>(config);
+        var actual = stamper.stampAndLoadAndExtract(template, factory.empty());
+        assertEquals(expected, actual);
+    }
+
+    /// A custom processor interface that defines methods to handle specific actions during document processing.
+    public interface ICustomProcessor {
+
+        /// This method is invoked to perform actions on a paragraph element if it holds a comment with the content
+        /// "`visitParagraph()`".
+        void visitParagraph();
+    }
+
+    /// CustomProcessor is a concrete implementation of the CommentProcessor abstract class and the ICustomProcessor
+    /// interface. It is designed to process comments and associated content within a paragraph in a custom manner.
+    ///
+    /// This class modifies the content of a paragraph when the `visitParagraph` method is invoked. Specifically, it
+    /// clears the content of the paragraph and replaces it with the word "Visited".
+    public static class CustomProcessor
+            extends CommentProcessor
+            implements ICustomProcessor {
+
+        CustomProcessor(ProcessorContext processorContext, PlaceholderReplacer placeholderReplacer) {
+            super(processorContext, placeholderReplacer);
+        }
+
+        @Override
+        public void visitParagraph() {
+            paragraph().apply(contentAccessor -> {
+                var content = contentAccessor.getContent();
+                content.clear();
+                content.add(newRun("Visited"));
+            });
+        }
+    }
+}

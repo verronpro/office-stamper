32cc169ecca5ce8ff812deec7cf5ecd0be4de315
Joseph Verron <joseph@verron.pro>
2025-12-05 00:52:59 +0800

    refactor(engine): simplify `PptxPart` by using compact constructor

--- engine/src/main/java/pro/verron/officestamper/experimental/PptxPart.java
+++ engine/src/main/java/pro/verron/officestamper/experimental/PptxPart.java
@@ -4,13 +4,4 @@ import org.docx4j.openpackaging.packages.PresentationMLPackage;
 
 /// The PptxPart class represents a specific implementation of the DocxPart interface designed for handling parts within
 /// a PowerPoint document.
-public record PptxPart(PresentationMLPackage document) {
-
-    /// Constructs a new instance of the PptxPart class.
-    ///
-    /// This constructor initializes an instance of PptxPart, which represents a specific implementation of the DocxPart
-    /// interface tailored for handling parts within a PowerPoint document. This class provides methods to interact with
-    /// and manipulate the content and structure of parts in a PowerPoint file.
-    public PptxPart {
-    }
-}
+public record PptxPart(PresentationMLPackage document) {}

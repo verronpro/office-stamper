6df1a8651af09a64b064e69dfbcdd354b73513fe
Joseph Verron <joseph@verron.pro>
2025-12-05 01:23:02 +0800

    refactor(api): remove `PlaceholderReplacer` interface and unused imports
    - Deleted `PlaceholderReplacer` interface as it is no longer used.
    - Cleaned up unused imports in `ParagraphRepeatProcessor`.

--- engine/src/main/java/pro/verron/officestamper/api/PlaceholderReplacer.java
+++
@@ -1,7 +0,0 @@
-package pro.verron.officestamper.api;
-
-import org.springframework.expression.EvaluationContext;
-
-public interface PlaceholderReplacer {
-    Insert resolve(DocxPart docxPart, String expression, EvaluationContext context);
-}

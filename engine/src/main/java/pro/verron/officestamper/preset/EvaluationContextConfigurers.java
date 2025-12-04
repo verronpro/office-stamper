c095aa7af532b71f4a4d1683e5942e8381508640
Joseph Verron <joseph@verron.pro>
2025-12-04 09:22:07 +0800

    refactor(engine): enhance security and clarify comments in EvaluationContextConfigurers
    - Updated DefaultEvaluationContextConfigurer to throw AccessException for unsupported bean resolution.
    - Reformatted and clarified JavaDoc comments to improve readability.
    - Enhanced security configuration in EvaluationContext setup.
    - Removed unnecessary suppression comments and improved documentation consistency.

--- engine/src/main/java/pro/verron/officestamper/preset/EvaluationContextConfigurers.java
+++ engine/src/main/java/pro/verron/officestamper/preset/EvaluationContextConfigurers.java
@@ -1,6 +1,7 @@
 package pro.verron.officestamper.preset;
 
 
+import org.springframework.expression.AccessException;
 import org.springframework.expression.EvaluationContext;
 import org.springframework.expression.TypeLocator;
 import org.springframework.expression.spel.SpelEvaluationException;
@@ -22,11 +23,9 @@ public class EvaluationContextConfigurers {
 
     /// Returns a [EvaluationContextConfigurer] instance that does no customization.
     ///
-    /// This configurer does nothing to the StandardEvaluationContext class, and therefore all the
-    /// unfiltered features are accessible.
-    /// It should be used when there is a need to use the
-    /// powerful features of the aforementioned class, and there is a trust that the template won't
-    /// contain any dangerous injections.
+    /// This configurer does nothing to the StandardEvaluationContext class, and therefore all the unfiltered features
+    /// are accessible. It should be used when there is a need to use the powerful features of the aforementioned class,
+    /// and there is a trust that the template won't contain any dangerous injections.
     ///
     /// @return a [EvaluationContextConfigurer] instance
     public static EvaluationContextConfigurer noopConfigurer() {
@@ -35,13 +34,11 @@ public class EvaluationContextConfigurers {
 
     /// Returns a default [EvaluationContextConfigurer] instance.
     ///
-    /// The default configurer provides better default security for the
-    /// [EvaluationContext] used by OfficeStamper.
-    /// It sets up the context with enhanced security measures, such as
-    /// limited property accessors, constructor resolvers, and method resolvers.
-    /// It also sets a type locator, type converter, type comparator, and operator overloader.
-    /// This configurer is recommended to be used when there is a need for improved security
-    /// and protection against potential dangerous injections in the template.
+    /// The default configurer provides better default security for the [EvaluationContext] used by OfficeStamper. It
+    /// sets up the context with enhanced security measures, such as limited property accessors, constructor resolvers,
+    /// and method resolvers. It also sets a type locator, type converter, type comparator, and operator overloader.
+    /// This configurer is recommended to be used when there is a need for improved security and protection against
+    /// potential dangerous injections in the template.
     ///
     /// @return a [EvaluationContextConfigurer] instance with enhanced security features
     public static EvaluationContextConfigurer defaultConfigurer() {
@@ -50,17 +47,15 @@ public class EvaluationContextConfigurers {
 
     /// [EvaluationContextConfigurer] that does no customization.
     ///
-    /// The NoOpEvaluationContextConfigurer is a configuration placeholder used to indicate the
-    /// intention to keep the standard powerful features provided by the
-    /// Spring framework's StandardEvaluationContext class.
+    /// The NoOpEvaluationContextConfigurer is a configuration placeholder used to indicate the intention to keep the
+    /// standard powerful features provided by the Spring framework's StandardEvaluationContext class.
     ///
-    /// StandardEvaluationContext is a powerful class by default, which can lead to potential security risks
-    /// if not properly managed. This might include potential dangerous injections in the template.
+    /// StandardEvaluationContext is a powerful class by default, which can lead to potential security risks if not
+    /// properly managed. This might include potential dangerous injections in the template.
     ///
-    /// This configurer does nothing to the StandardEvaluationContext class, and therefore all the
-    /// unfiltered features are accessible. It should be used when there is a need to use the
-    /// powerful features of the aforementioned class, and there is a trust that the template won't
-    /// contain any dangerous injections.
+    /// This configurer does nothing to the StandardEvaluationContext class, and therefore all the unfiltered features
+    /// are accessible. It should be used when there is a need to use the powerful features of the aforementioned class,
+    /// and there is a trust that the template won't contain any dangerous injections.
     ///
     /// @author Joseph Verron
     /// @author Mario Siegenthaler
@@ -78,15 +73,15 @@ public class EvaluationContextConfigurers {
         }
     }
 
-    /// [EvaluationContextConfigurer] that has better default security,
-    /// especially doesn't allow especially known injections.
+    /// [EvaluationContextConfigurer] that has better default security, especially doesn't allow especially known
+    /// injections.
     ///
     /// @author Joseph Verron
     /// @version ${version}
     /// @since 1.6.5
     private static class DefaultEvaluationContextConfigurer
             implements EvaluationContextConfigurer {
-        /// {@inheritDoc}
+
         @Override
         public void configureEvaluationContext(StandardEvaluationContext context) {
             TypeLocator typeLocator = typeName -> {
@@ -95,8 +90,9 @@ public class EvaluationContextConfigurers {
             context.setPropertyAccessors(List.of(DataBindingPropertyAccessor.forReadWriteAccess(), new MapAccessor()));
             context.setConstructorResolvers(Collections.emptyList());
             context.setMethodResolvers(new ArrayList<>(List.of(DataBindingMethodResolver.forInstanceMethodInvocation())));
-            //noinspection DataFlowIssue, ignore the warning since it is a workaround fixing potential security issues
-            context.setBeanResolver(null);
+            context.setBeanResolver((_, _) -> {
+                throw new AccessException("Bean resolution not supported for security reasons.");
+            });
             context.setTypeLocator(typeLocator);
             context.setTypeConverter(new StandardTypeConverter());
             context.setTypeComparator(new StandardTypeComparator());

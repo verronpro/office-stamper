366f1d371411c75e972dc415d0c4a550f7829482
jushar <4458720+jushar@users.noreply.github.com>
2026-02-11 12:37:14 +0100

    test: add test for multiple images using #this

--- engine/src/test/java/pro/verron/officestamper/test/ProcessorRepeatDocPartTest.java
+++ engine/src/test/java/pro/verron/officestamper/test/ProcessorRepeatDocPartTest.java
@@ -17,10 +17,7 @@ import pro.verron.officestamper.test.utils.ObjectContextFactory;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.Map;
-import java.util.Queue;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -683,6 +680,26 @@ class ProcessorRepeatDocPartTest {
                 Always rendered:
                 
                 /word/media/document_image_rId13.png:rId13:image/png:193.6 kB:sha1=t8UNAmo7yJgZJk9g7pLLIb3AvCA=:cy=$d:6120130
+                
+                
+                
+                """, actual);
+    }
+
+    @MethodSource("factories")
+    @DisplayName("Repeat doc part specifications with #self")
+    @Test
+    void shouldImportImageDataWithThisInTheMainDocument() {
+        var stamper = docxPackageStamper(standard());
+        var stamped = stamper.stamp(getWordResource(Path.of("ProcessorRepeatDocPart_Image2.docx")),
+                Map.of("images", List.of(getImage(Path.of("butterfly.png")), getImage(Path.of("map.jpg")))));
+        var actual = docxToString(stamped);
+        assertEquals("""
+                
+                
+                /word/media/document_image_rId11.png:rId11:image/png:193.6 kB:sha1=t8UNAmo7yJgZJk9g7pLLIb3AvCA=:cy=$d:6120130
+                
+                /word/media/document_image_rId12.jpeg:rId12:image/jpeg:407.5 kB:sha1=Ujo3UzL8WmeZN/1K6weBydaI73I=:cy=$d:6120130
                 
                 
                 
